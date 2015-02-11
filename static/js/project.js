798b3034f698b268d078e8ef553e1adf1ddb04b3
Pete Booth <petebooth@Petes-MBP-3.home>
2015-02-11 21:14:35 +0000

    Tidying up and setting up compass

--- static/js/project.js
+++ static/js/project.js
@@ -1,23 +1,21 @@
-/* Project specific Javascript goes here. */
-
-var timeoutdelay = 333;
+var timeoutdelay = 2000;  
 $(document).ready(function(){
   $('.control').click(function(){
-    var id = this.id;
-    var cube = $('.cubecontainer > div');
-    cube.removeAttr('class');
-    //first we need to back the cube away from the viewport in its current orientation
-    cube.addClass('show-' + cube.attr('data-orientation'));
-    cube.attr('data-orientation',id);
-    window.setTimeout(function(){
-        //then we need to rotate it
-        cube.removeAttr('class');
-        cube.addClass('show-' + id );
-        window.setTimeout(function(){
-          //finally we move it back to align with the viewport
+      var id = this.id;
+      var cube = $('.cubecontainer > div');
+      cube.removeAttr('class');
+      //first we need to back the cube away from the viewport in its current orientation
+      cube.addClass('show-' + cube.attr('data-orientation') + '-face');
+      cube.attr('data-orientation',id);
+      window.setTimeout(function(){
+          //then we need to rotate it
           cube.removeAttr('class');
-          cube.addClass('show-' + id +'-face');
-        },timeoutdelay);
-    },timeoutdelay);
-  })
-});
+          cube.addClass('show-' + id + '-face' );
+          window.setTimeout(function(){
+            //finally we move it back to align with the viewport
+            cube.removeAttr('class');
+            cube.addClass('show-' + id);
+          },timeoutdelay);
+      },timeoutdelay);
+    })
+})
